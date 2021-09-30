d57683fa326b0211bc294540b832c254760a612d
yuki-zirushi <81721112+yuki-zirushi@users.noreply.github.com>
2021-09-30 15:46:31 +0900

    [Create]

---
+++ LocalDateTime.java
@@ -0,0 +1,11 @@
+import java.time.*;
+
+public class Main {
+  public static void main(String[] args) {
+    LocalDateTime l1 = LocalDateTime.now();
+    LocalDateTime l2 = LocalDateTime.of(2021, 1, 1, 9, 5, 0, 0);
+
+    ZonedDateTime z1 = l2.atZone(ZoneId.of("Europe/London"));
+    LocalDateTime l3 = z1.toLocalDateTime();
+  }
+}
