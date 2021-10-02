99113e4052868be75ac684fb66c317fc46f77db8
yuki-zirushi <81721112+yuki-zirushi@users.noreply.github.com>
2021-10-02 20:00:34 +0900

    [Create]

---
+++ calcapp.java
@@ -0,0 +1,10 @@
+package calcapp.main;
+import calcapp.logics.CalcLogic;
+
+public class Calc {
+  public static void main(String[] args) {
+
+    int total = CalcLogic.tasu(a, b);
+    int delta = calcapp.logics.CalcLogic.hiku(a, b);
+  }
+}
